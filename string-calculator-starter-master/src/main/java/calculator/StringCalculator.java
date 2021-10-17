de7fdf8590f28b14569acf9c780210d41e690174
D6_49786_AkshayShebe <akshay.shebe@gmail.com>
2021-10-17 22:03:20 +0530

    method can take up to two numbers, separated by commas, and will return their sum

--- string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -2,10 +2,23 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    	if(input.isEmpty())
-        return 0;
+    // The method can take up to two numbers, separated by commas, and will return their sum. 
+	
+	public int add(String input) {
+		
+		String[] numbers = input.split(",");
+		
+    	if(input == null || input.isEmpty()) {
+    		return 0;
+    	} 
     	
-    	return Integer.parseInt(input);
+	    else if(input.length() == 1) {
+    		return Integer.parseInt(input);
+    	}  
+    	else {
+    		
+    		return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+    	}
+
     }
 }
